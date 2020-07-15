c94dc0b980b3a6e52ac2e905df924ecbeb6792df
manasadusa <manasajntuh13519@gmail.com>
2020-07-15 15:07:59 +0530

    Updates

---
+++ precache-manifest.698287c13c86bde70190b804b186841a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8bad64edf133fa8d269",
+    "url": "/User-profile/static/css/main.e8a3aaa9.chunk.css"
+  },
+  {
+    "revision": "a8bad64edf133fa8d269",
+    "url": "/User-profile/static/js/main.a8bad64e.chunk.js"
+  },
+  {
+    "revision": "505b45f2b88e909acd7f",
+    "url": "/User-profile/static/js/runtime~main.505b45f2.js"
+  },
+  {
+    "revision": "c776652202af5a1a2e94",
+    "url": "/User-profile/static/js/2.c7766522.chunk.js"
+  },
+  {
+    "revision": "78b7986f978986c7616ccb302e641082",
+    "url": "/User-profile/static/media/background.78b7986f.jpg"
+  },
+  {
+    "revision": "8e1af4db8afdaa9d0ade813b1a936408",
+    "url": "/User-profile/index.html"
+  }
+];
